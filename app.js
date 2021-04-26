6dcba3d13cfa16c6b5a74d8798d96abeab065186
Timo Koskinen <koskinentht@gmail.com>
2021-04-26 23:54:40 +0300

    all routes basic functionality working, basic templates finished

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var books = require('./routes/books');
 
 var app = express();
 
@@ -19,33 +19,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/books', books);
 const {sequelize} = require('./models');
 
-(async function() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+// Connection testing script:
+// (async function() {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// })();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// catch 404
+app.use(function(req, res, next) {  
+  const error = new Error();
+  error.status = 404;
+  error.message = 'Page Not Found!';
+  next(error);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function(err, req, res, next) {  
+  if (err.status === 404) {
+    res.status(err.status);
+    res.render('error', {message: err.message, error: err, title:"Not Found!"})
+  } else {
+    res.status(err.status || 500);
+    err.message = err.message || "Something went wrong!";
+    res.render('error', {message: err.message, error: err, title: "Error!"})
+  }
 });
 
 module.exports = app;
---
+++ routes/books.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+const Book = require('../models').Book;
+
+function asyncWrapper(cb) {
+  return async(req,res,next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+/* GET books listing. */
+router.get('/', asyncWrapper(async (req, res, next) => {
+  let books = await Book.findAll();
+  res.render('books/index', {title: "Books", books});
+}));
+
+router.get('/new', function(req, res, next) {
+  res.render('books/new', {book: {}, title:"New Book"});
+});
+
+router.post('/new', asyncWrapper(async (req, res, next) => {
+  let book = await Book.create(req.body);
+  res.redirect('/books');
+}));
+
+router.get('/:id', asyncWrapper(async (req, res, next) => {
+  let id = req.params.id;
+  let book = await Book.findByPk(id);
+  res.render('books/update', {book, title:"Update Book"});
+}));
+
+router.post('/:id', asyncWrapper(async (req, res, next) => {
+  let id = req.params.id;
+  let book = await Book.findByPk(id);
+  await book.update(req.body);
+  res.redirect('/');
+}));
+
+router.get('/:id/delete', asyncWrapper(async (req, res, next) => {
+  let id = req.params.id;
+  let book = await Book.findByPk(id);
+  res.render('books/delete', {book, title:"Delete Book"})
+}));
+
+router.post('/:id/delete', asyncWrapper(async (req, res, next) => {
+  let id = req.params.id;
+  let book = await Book.findByPk(id);
+  await book.destroy();
+  res.redirect('/');
+}));
+
+module.exports = router;
